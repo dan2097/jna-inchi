9e834d3858ccbbe605ffa5b2de7d9129e220fe86
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2021-12-05 17:10:20 +0000

    Update to JUnit5

--- jna-inchi-smiles/src/test/java/com/github/dan2097/jnainchi/SmilesToInchiTest.java
+++ jna-inchi-smiles/src/test/java/com/github/dan2097/jnainchi/SmilesToInchiTest.java
@@ -19,35 +19,23 @@ package com.github.dan2097.jnainchi;
 
 import static org.junit.Assert.fail;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SmilesToInchiTest {
 
-	@Test
-	public void testConversion() throws IOException{
-      try (BufferedReader input = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("smilesintegrationtests.smi"), "UTF-8"))){
-        String line = null;
-        while ((line = input.readLine()) != null) {
-            if(line.isEmpty() || line.startsWith("#")){
-                continue;
-            }
-            String[] lineArray = line.split("\t");
-            String inchi = SmilesToInchi.toInchi(lineArray[0]).getInchi();
-            if (inchi!=null) {
-                String referenceInchi = lineArray[1];
-
-                if (!inchi.equals(referenceInchi)){
-                    fail(lineArray[0] +" was misconverted as: " + inchi);
-                }
-            } else {
-                fail(lineArray[0] +" was not converted");
-            }
-        }
+	@ParameterizedTest
+	@CsvFileSource(resources = "smilesintegrationtests.smi", delimiter='\t')
+    public void testConversion(String smiles, String expectedInchi) throws IOException {
+      String inchi = SmilesToInchi.toInchi(smiles).getInchi();
+      if (inchi!=null) {
+          if (!inchi.equals(expectedInchi)){
+              fail(smiles +" was misconverted as: " + inchi);
+          }
+      } else {
+          fail(smiles +" was not converted");
       }
-	}
-
+    }
 }
