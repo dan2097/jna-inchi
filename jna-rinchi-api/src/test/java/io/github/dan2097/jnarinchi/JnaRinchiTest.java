fd33b8fca1395831b7d879ebdfad38bd693edf88
ntk73 <nick@uni-plovdiv.net>
2022-06-17 12:00:30 +0300

    Added new test class JnaRinchiTest

---
+++ jna-rinchi-api/src/test/java/io/github/dan2097/jnarinchi/JnaRinchiTest.java
@@ -0,0 +1,13 @@
+package io.github.dan2097.jnarinchi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class JnaRinchiTest 
+{
+	@Test 
+	public void testDummy() {
+		assertTrue(true);
+	}
+}
