11e93e73c4bb24b573f06eb39d92a9fdebb02af8
ntk73 <nick@uni-plovdiv.net>
2022-06-21 15:18:20 +0300

    Added shortDesignation in RinchiKeyType enum

--- jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/RinchiOptions.java
+++ jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/RinchiOptions.java
@@ -20,7 +20,19 @@ package io.github.dan2097.jnarinchi;
 public class RinchiOptions 
 {
 	public static enum RinchiKeyType {
-		LONG, SHORT, WEB
+		LONG ("L"), 
+		SHORT ("S"), 
+		WEB ("W");
+		
+		private final String shortDeignation;
+		
+		private RinchiKeyType (String shortDeignation) {
+			this.shortDeignation = shortDeignation; 
+		}
+		
+		public String getShortDeignation() {
+			return shortDeignation;
+		}
 	}
 	
 	private RinchiKeyType rinchiKeyType = RinchiKeyType.LONG;
