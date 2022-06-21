9907c9299123854d1eea217c392fc7a5427dff50
ntk73 <nick@uni-plovdiv.net>
2022-06-21 15:12:35 +0300

    Added new class(enum) RinchiStatus

---
+++ jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/RinchiStatus.java
@@ -0,0 +1,29 @@
+/**
+ * JNA-RInChI - Library for calling RInChI from Java
+ * Copyright © 2022 Nikolay Kochev
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.dan2097.jnarinchi;
+
+public enum RinchiStatus {
+	/** Success; no errors or warnings */
+	SUCCESS,
+
+	/** Success; warning(s) issued */
+	WARNING,
+
+	/** Error: no InChI has been created */
+	ERROR,
+}
