9c1f5165524fe1d72a170b6bd76d729664e8b16d
ntk73 <nick@uni-plovdiv.net>
2022-07-19 17:17:45 +0300

    Added new class RinchiDecompositionOutput

---
+++ jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/RinchiDecompositionOutput.java
@@ -0,0 +1,71 @@
+/**
+ * JNA-RInChI - Library for calling RInChI from Java
+ * Copyright © 2022 Nikolay Kochev
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.dan2097.jnarinchi;
+
+
+public class RinchiDecompositionOutput 
+{			
+	private final int direction;		
+	private final String[] inchies;
+	private final String[] auxInfos;
+	private final ReactionComponentRole[] roles;
+	private final RinchiDecompositionStatus status;
+	private final int errorCode;
+	private final String errorMessage;
+	
+	
+	public RinchiDecompositionOutput (int direction, String[] inchies, String[] auxInfos, ReactionComponentRole[] roles,
+			RinchiDecompositionStatus status, int errorCode, String errorMessage) 
+	{
+		this.direction = direction;
+		this.inchies = inchies;
+		this.auxInfos = auxInfos;
+		this.roles = roles;
+		this.status = status;
+		this.errorCode = errorCode;
+		this.errorMessage =errorMessage;
+	}
+
+	public int getDirection() {
+		return direction;
+	}
+
+	public String[] getInchies() {
+		return inchies;
+	}
+
+	public String[] getAuxInfos() {
+		return auxInfos;
+	}
+
+	public ReactionComponentRole[] getRoles() {
+		return roles;
+	}
+
+	public RinchiDecompositionStatus getStatus() {
+		return status;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}	
+}
