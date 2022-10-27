2a2fb0153200816494cad172292019bc326db0e2
ntk73 <nick@uni-plovdiv.net>
2022-10-27 17:15:13 +0300

    Added new class Output

---
+++ jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/Output.java
@@ -0,0 +1,44 @@
+/**
+ * JNA-RInChI - Library for calling RInChI from Java
+ * Copyright © 2022 Nikolay Kochev
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.dan2097.jnarinchi;
+
+/**
+ * This class is a placeholder for the generic output/result from 
+ * RInChI library operation.
+ * 
+ * @author Nikolay Kochev
+ *
+ */
+public class Output {
+	
+	protected final Status status = Status.SUCCESS;
+	protected final int errorCode = 0;
+	protected final String errorMessage = "";
+		
+	public Status getStatus() {
+		return status;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+}
