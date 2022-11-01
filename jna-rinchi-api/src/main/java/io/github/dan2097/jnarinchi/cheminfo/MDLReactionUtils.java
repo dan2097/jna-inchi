67e4dd336794d54a47ed571db788f3b432d2da1d
Nikolay Kochev <nick@uni-plovdiv.net>
2022-11-01 09:48:19 +0200

    Added javadocs fir class MDLReactionUtils

--- jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/cheminfo/MDLReactionUtils.java
+++ jna-rinchi-api/src/main/java/io/github/dan2097/jnarinchi/cheminfo/MDLReactionUtils.java
@@ -4,7 +4,12 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 import io.github.dan2097.jnainchi.InchiAtom;
-
+/**
+ * Some helpers (utils) for handling reaction reading and writing 
+ * in MDL formats
+ * @author nick
+ *
+ */
 public class MDLReactionUtils {
 	public static final int MDL_FLOAT_SPACES = 10;
 	public static final String CTAB_LINE_COUNT = "999";
@@ -18,6 +23,12 @@ public class MDLReactionUtils {
 		MDL_NUMBER_FORMAT.setGroupingUsed(false);
 	}
 	
+	/**
+	 * Returns implicit hydrogen atom coding according to the MDL syntax.
+	 * 
+	 * @param atom atom represented as an object of the type {@link InchiAtom}
+	 * @return H atoms coding as an integer
+	 */
 	public static int getImplicitHAtomCoding(InchiAtom atom) {
 		//Implicit H atoms coding: 1 = H0, 2 = H1, 3 = H2, 4 = H3, 5 = H4
 		if (atom.getImplicitHydrogen() == 0)
